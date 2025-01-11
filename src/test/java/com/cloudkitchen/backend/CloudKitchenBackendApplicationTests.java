c511b7c5b2ecfa5a41a4e9fdce6fb1079c38c65a
Sneha Soni <snehasoni@Snehas-MacBook-Air.local>
2025-01-12 02:17:05 +0530

    Initial commit

---
+++ src/test/java/com/cloudkitchen/backend/CloudKitchenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudkitchen.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudKitchenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
