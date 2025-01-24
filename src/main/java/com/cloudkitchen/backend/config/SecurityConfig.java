45e2f0e17aac48249324e37c94ef871d9a79eb3f
Sneha Soni <snehasoni@Snehas-MacBook-Air.local>
2025-01-24 20:46:29 +0530

    InMemoryAuthorization

--- src/main/java/com/cloudkitchen/backend/config/SecurityConfig.java
+++ src/main/java/com/cloudkitchen/backend/config/SecurityConfig.java
@@ -1,27 +1,43 @@
 package com.cloudkitchen.backend.config;
 
-import com.cloudkitchen.backend.service.MenuCardsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/menucards/**").hasRole("ADMIN")
-                        .requestMatchers("/auth").permitAll()
+                        .requestMatchers("/api/v1/menucards/**").permitAll()
                         .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails user = User.builder().username("Sneha").password(passwordEncoder().encode("Sneha")).roles("USER").build();
+        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
 }
